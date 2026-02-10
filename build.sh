da197ab7394156cb9412bbaac1db4792b31b5235
Shabbirh10 <112505622+Shabbirh10@users.noreply.github.com>
2026-02-10 11:33:38 +0530

    Prepare for Render deployment: Add build scripts, config, and dependencies

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Install dependencies
+poetry install
+
+# Convert static asset files
+poetry run python manage.py tailwind install
+poetry run python manage.py tailwind build
+poetry run python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+poetry run python manage.py migrate
--- django_finance/config/settings/base.py
+++ django_finance/config/settings/base.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 
 from django.contrib.messages import constants as messages
+import dj_database_url
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -32,7 +33,8 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-#_!@")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+
 
 
 # Application definition
@@ -68,6 +70,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -103,10 +106,10 @@ WSGI_APPLICATION = "django_finance.config.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 
@@ -155,6 +158,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
